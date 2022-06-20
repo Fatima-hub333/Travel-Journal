9bb794928602819c9d29dc41a743b82b88a86c80
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-20 12:52:02 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
+import React from 'react'
+import Navbar from './components/Navbar'
+import Cards from './components/Cards'
+import Data from './Data'
 import './App.css';
 
 export default function App() {
+  const listOfCards = Data.map((CardsData) => {
+    return <Cards items={CardsData} />
+  })
+
   return (
-    <div>
-      
+    <div className="main">
+      <Navbar />
+      <div className="list-cards">
+        {listOfCards}
+      </div>
     </div>
   )
 }
